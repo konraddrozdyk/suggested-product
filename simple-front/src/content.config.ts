3f87abc1c3a384d815f4d66dffcf1e5c91a8cb6c
konraddrozdyk <konrad.drozdyk@gmail.com>
2024-12-12 11:09:44 +0100

    Added a data foler for products

---
+++ simple-front/src/content.config.ts
@@ -0,0 +1,17 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content";
+
+// 2. Import loader(s)
+import { glob, file } from "astro/loaders";
+
+// 3. Define your collection(s)
+const products = defineCollection({
+  // Define a loader for this collection
+  loader: glob({ pattern: "**/*.json", base: "./src/data/products" }),
+  schema: z.object({
+    name: z.string(),
+  }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { products };
